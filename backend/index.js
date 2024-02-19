9fb1fac9f3f0880427369122feaf85941b6094c9
Arham Khawar <68909415+ch-Arham@users.noreply.github.com>
2024-02-19 19:06:19 +0500

    server initialize- server and socket.io setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,37 @@
-console.log("Helo from backend!")
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+const PORT = 5000;
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:5173",
+    },
+});
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
+
+io.on("connection", (socket) => {
+    console.log("User Connected", socket.id);
+
+    socket.on("message", ({ room, message }) => {
+        console.log({ room, message });
+        socket.to(room).emit("receive-message", message);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
+});
+
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
